43ad4b07610577fd8b730361dd3307af246691ef
Kristian Heljas <kristian@kristian.ee>
2023-03-08 18:38:45 +0200

    Refactor build script to facilitate upcoming file watcher
    - Move build code into a class and separate build stages
    - Close yaml files after paring using context manager

--- build.py
+++ build.py
@@ -1,31 +1,64 @@
 import os
 import shutil
+import sys
 import sass
-
+import yaml
 from pathlib import Path
 from staticjinja import Site
-from yaml import Loader, load
 
-def data():
-    return {
-        "base": load(open("data/base.yaml"), Loader=Loader),
-        "links": load(open("data/links.yaml"), Loader=Loader),
-        "pages": load(open("data/pages.yaml"), Loader=Loader),
-        "projects": load(open("data/projects.yaml"), Loader=Loader)
-    }
 
-buildpath = Path('build')
-if buildpath.exists() and buildpath.is_dir():
-    shutil.rmtree("build")
+def load_yaml_file(file_path):
+    with open(file_path, 'r') as file:
+        return yaml.load(file.read(), Loader=yaml.Loader)
+
+
+def print_stage_header(header):
+    sys.stderr.write('=== {} === \n'.format(header))
+
+
+class DocSiteBuilder:
+    def __init__(self, build_dir='build'):
+        self.build_dir = build_dir
+
+    def build(self, clean=True):
+        if clean:
+            self.clean()
+
+        self.build_html()
+        self.build_assets()
+        self.build_stylesheets()
+
+    def build_html(self):
+        print_stage_header('rendering jinja templates')
+        site = Site.make_site(
+            searchpath=os.path.abspath('templates'),
+            outpath=os.path.abspath(self.build_dir),
+            contexts=[(".*.html", self.jinja_globals)]
+        )
+        site.render()
+
+    def build_assets(self):
+        print_stage_header('copying static assets')
+        shutil.copytree('static', os.path.join(self.build_dir, 'static'), dirs_exist_ok=True)
+
+    def build_stylesheets(self):
+        print_stage_header('compiling stylesheets')
+        sass.compile(dirname=('sass', os.path.join(self.build_dir, 'static/css')))
+
+    def clean(self):
+        buildpath = Path(self.build_dir)
+        if buildpath.exists() and buildpath.is_dir():
+            shutil.rmtree(self.build_dir)
+
+    def jinja_globals(self):
+        return {
+            "base": load_yaml_file("data/base.yaml"),
+            "links": load_yaml_file("data/links.yaml"),
+            "pages": load_yaml_file("data/pages.yaml"),
+            "projects": load_yaml_file("data/projects.yaml")
+        }
+
 
 if __name__ == "__main__":
-
-    site = Site.make_site()
-    site.outpath="build"
-    site.contexts=[(".*.html", data)]
-    # disable automatic reloading
-    site.render(use_reloader=False)
-
-
-    shutil.copytree('static', 'build/static')
-    sass.compile(dirname=('sass', 'build/static/css'))
+    builder = DocSiteBuilder()
+    builder.build()
